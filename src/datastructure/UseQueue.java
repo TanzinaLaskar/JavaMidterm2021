b84f2b00fac49c6bcc84507576f6f44ae4786fab
Tanzina Laskar <tanzinalaskar@Tanzinas-MacBook-Pro.local>
2021-05-03 00:31:35 -0400

    initial commit

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,6 +1,17 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
+
+
+    /*
+     * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
+     * Use For Each loop and while loop with Iterator to retrieve data.
+     *
+     */
 
     public static void main(String[] args) {
         /*
@@ -9,6 +20,33 @@ public class UseQueue {
          *
          */
 
+        Queue<String> queueList = new LinkedList<>();
+        queueList.add("Tanzina");
+        queueList.add("Farzin");
+        queueList.add("Shadman");
+        queueList.add("Thamina");
+
+        queueList.remove(3);
+        System.out.println("available variables are: " + queueList);
+
+        System.out.println(queueList.poll());
+        System.out.println(queueList.poll());
+        System.out.println(queueList.peek());
+        System.out.println(queueList.poll());
+
+        for (
+                String item : queueList)
+            System.out.print(item + ", ");
+
+        System.out.print("\nThe Queue items using iterator: ");
+        Iterator<String> iter = queueList.iterator();
+        while (iter.hasNext()) {
+            System.out.print(iter.next() + ", ");
+
+
+        }
     }
 
 }
+
+
